e47dc70adaa71532737256969bf2d1d3593caa62
Tim Uruski <hey@tim.uru.ski>
2020-08-13 08:49:46 -0600

    Refactor and add progress bar

--- ppm.rb
+++ ppm.rb
@@ -3,18 +3,37 @@
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 
-print "P3\n#{IMG_WIDTH} #{IMG_HEIGHT}\n255\n"
+def ppm(width, height)
+  # Clear before printing status output
+  # $stderr.print "\e[0;0H\e[K\e[0m"
+  $stderr.print "\e[2J"
 
-(IMG_HEIGHT - 1).downto(0) do |j|
-  0.upto(IMG_WIDTH - 1) do |i|
-    r = i.to_f / (IMG_WIDTH - 1)
-    g = j.to_f / (IMG_HEIGHT - 1)
-    b = 0.25
+  # PPM header
+  $stdout.puts "P3\n#{IMG_WIDTH} #{IMG_HEIGHT}\n255"
 
-    ir = (255.999 * r).to_i
-    ig = (255.999 * g).to_i
-    ib = (255.999 * b).to_i
+  (height - 1).downto(0) do |j|
+    $stderr.print "\e[0;0H"
+    $stderr.puts "Scanlines remaining: #{j}"
+    $stderr.flush
 
-    print "#{ir} #{ig} #{ib}\n"
+    0.upto(width - 1) do |i|
+      r, g, b = yield i, j
+
+      $stdout.puts "#{r} #{g} #{b}"
+    end
   end
+
+  $stderr.print "\e[0;0H\e[KDone!\n"
+end
+
+ppm(IMG_WIDTH, IMG_HEIGHT) do |i, j|
+  r = i.to_f / (IMG_WIDTH - 1)
+  g = j.to_f / (IMG_HEIGHT - 1)
+  b = 0.25
+
+  ir = (255.999 * r).to_i
+  ig = (255.999 * g).to_i
+  ib = (255.999 * b).to_i
+
+  [ir, ig, ib]
 end
