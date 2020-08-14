82bba9246727922de115c2d9d31c9b34520bdb83
Tim Uruski <hey@tim.uru.ski>
2020-08-14 12:59:00 -0600

    Pull up renderer class, improve render progress output

--- ray_tracer.rb
+++ ray_tracer.rb
@@ -17,7 +17,7 @@ at_exit do
 
   camera = Camera.new
 
-  ppm(IMAGE_WIDTH, IMAGE_HEIGHT) do |i, j|
+  Renderer.new(IMAGE_WIDTH, IMAGE_HEIGHT).ppm do |i, j|
     pixel_color = Color.new(0,0,0)
     SAMPLES_PER_PIXEL.times do
       u = (i.to_f + rand) / (IMAGE_WIDTH - 1)
@@ -30,24 +30,6 @@ at_exit do
   end
 
   $stderr.print "\e[0;0H✨ Finished in #{duration(start_time)}!\e[K\n"
-end
-
-def ppm(width, height)
-  # Clear before printing status output
-  $stderr.print "\e[2J"
-
-  # PPM header
-  $stdout.puts "P3\n#{width} #{height}\n255"
-
-  (height - 1).downto(0) do |j|
-    0.upto(width - 1) do |i|
-      start_time = Time.now
-      $stdout.puts yield i, j
-
-      $stderr.print progress(width, height, i, j, start_time)
-      $stderr.flush
-    end
-  end
 end
 
 def ray_color(r, world, depth)
@@ -64,20 +46,45 @@ def ray_color(r, world, depth)
   end
 end
 
-def progress(width, height, i, j, start_time)
-  total = (height * width)
-  complete = (j * width + i)
-  percent = 100.0 - (complete.to_f / total.to_f * 100)
-
-  remaining = (Time.now - start_time) * (total - complete)
-
-  "\e[0;0H⏳ Rendering #{"%4.2f" % percent}%, estimated remaining: #{"%4d" % remaining} sec...\e[K"
-end
-
 def duration(start_time, finish_time = Time.now)
   diff = (finish_time.to_i - start_time.to_i)
   min, rest = diff.divmod(60)
   sec, _ = rest.divmod(1)
 
   "#{min} min, #{sec} sec"
+end
+
+class Renderer
+  attr_reader :width, :height, :total
+
+  def initialize(width, height)
+    @width = width
+    @height = height
+    @total = width * height
+  end
+
+  def ppm
+    # Clear before printing status output
+    $stderr.print "\e[2J"
+
+    # PPM header
+    $stdout.puts "P3\n#{width} #{height}\n255"
+
+    (height - 1).downto(0) do |j|
+      0.upto(width - 1) do |i|
+        start_time = Time.now
+        $stdout.puts yield i, j
+
+        $stderr.print progress(i, j, start_time)
+        $stderr.flush
+      end
+    end
+  end
+
+  def progress(i, j, start_time)
+    complete = (height - j) * width + (i + 1)
+    percent = (complete.to_f / total.to_f * 100)
+    remaining = (Time.now - start_time) * (total - complete)
+    "\e[0;0H⏳ Rendering #{"%4.2f" % percent}%, estimated remaining: #{"%3d" % remaining} sec...\e[K"
+  end
 end
