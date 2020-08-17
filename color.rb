6a7b35e0c02733ab6bd262a72d55038c4ac5e1ae
Tim Uruski <hey@tim.uru.ski>
2020-08-17 09:02:18 -0600

    Prepare for parallel processing

--- color.rb
+++ color.rb
@@ -1,12 +1,12 @@
 require_relative "vec3"
 
 Color = Class.new(Vec3) do
-  def to_s(samples = 1)
+  def self.format(color, samples = 1)
     # Divide the color by the number of samples and gamma-correct for gamma=2.0.
     scale = 1.0 / samples
-    r = Math.sqrt(scale * x)
-    g = Math.sqrt(scale * y)
-    b = Math.sqrt(scale * z)
+    r = Math.sqrt(scale * color.x)
+    g = Math.sqrt(scale * color.y)
+    b = Math.sqrt(scale * color.z)
 
     r = (256 * r.clamp(0.0, 0.999)).to_i
     g = (256 * g.clamp(0.0, 0.999)).to_i
@@ -14,8 +14,4 @@ Color = Class.new(Vec3) do
 
     "#{r} #{g} #{b}"
   end
-end
-
-def Color(v)
-  Color.new(v.x, v.y, v.z)
 end
--- ray_tracer.rb
+++ ray_tracer.rb
@@ -2,8 +2,8 @@
 
 require_relative "core"
 
-ASPECT_RATIO = 3.0 / 2.0
-IMAGE_WIDTH = 1200
+ASPECT_RATIO = 16.0 / 9.0
+IMAGE_WIDTH = 800
 IMAGE_HEIGHT = (IMAGE_WIDTH / ASPECT_RATIO).to_i
 SAMPLES_PER_PIXEL = (ENV["SAMPLES"] || 1).to_i # 100 samples takes ~2 minutes
 MAX_DEPTH = 50
@@ -20,6 +20,9 @@ at_exit do
   aperture = 0.1
   camera = Camera.new(look_from, look_at, vup, 20.0, ASPECT_RATIO, aperture, dist_to_focus)
 
+  # Clear before printing status output
+  $stderr.print "\e[2K"
+
   Renderer.new(IMAGE_WIDTH, IMAGE_HEIGHT).ppm do |i, j|
     pixel_color = Color.new(0,0,0)
     SAMPLES_PER_PIXEL.times do
@@ -29,7 +32,7 @@ at_exit do
       pixel_color += ray_color(r, world, MAX_DEPTH)
     end
 
-    pixel_color.to_s(SAMPLES_PER_PIXEL)
+    Color.format(pixel_color, SAMPLES_PER_PIXEL)
   end
 
   $stderr.print "\e[0;0H✨ Finished in #{duration(start_time)}!\e[K\n"
@@ -41,37 +44,37 @@ def random_scene
   ground_material = Lambertian.new(Color.new(0.5, 0.5, 0.5))
   world.push Sphere.new(Point3.new(0,-1000,0), 1000.0, ground_material)
 
-  -11.upto(10) do |a|
-    -11.upto(10) do |b|
-      choose_mat = rand
-      center = Point3.new(a + 0.8 * rand, 0.2, b + 0.9 * rand)
+  # -11.upto(10) do |a|
+  #   -11.upto(10) do |b|
+  #     choose_mat = rand
+  #     center = Point3.new(a + 0.8 * rand, 0.2, b + 0.9 * rand)
 
-      if (center - Point3.new(4, 0.2, 0)).length > 0.9
-        if choose_mat < 0.8
-          # diffuse
-          albedo = Color.random * Color.random
-          sphere_material = Lambertian.new(albedo)
-          world.push Sphere.new(center, 0.2, sphere_material)
-        elsif choose_mat < 0.95
-          # metallic
-          albedo = Color.random(0.5, 1)
-          fuzz = rand(0.0..0.5)
-          sphere_material = Metal.new(albedo, fuzz)
-          world.push Sphere.new(center, 0.2, sphere_material)
-        else
-          # glass
-          sphere_material = Dielectric.new(1.5)
-          world.push Sphere.new(center, 0.2, sphere_material)
-        end
-      end
-    end
-  end
+  #     if (center - Point3.new(4, 0.2, 0)).length > 0.9
+  #       if choose_mat < 0.8
+  #         # diffuse
+  #         albedo = Color.random * Color.random
+  #         sphere_material = Lambertian.new(albedo)
+  #         world.push Sphere.new(center, 0.2, sphere_material)
+  #       elsif choose_mat < 0.95
+  #         # metallic
+  #         albedo = Color.random(0.5, 1)
+  #         fuzz = rand(0.0..0.5)
+  #         sphere_material = Metal.new(albedo, fuzz)
+  #         world.push Sphere.new(center, 0.2, sphere_material)
+  #       else
+  #         # glass
+  #         sphere_material = Dielectric.new(1.5)
+  #         world.push Sphere.new(center, 0.2, sphere_material)
+  #       end
+  #     end
+  #   end
+  # end
 
   material1 = Dielectric.new(1.5)
-  world.push Sphere.new(Point3.new(0,1,0), 1.0, material1)
+  world.push Sphere.new(Point3.new(0,1,0.0), 1.0, material1)
 
   material2 = Lambertian.new(Color.new(0.4,0.2,0.1))
-  world.push Sphere.new(Point3.new(-4,1,0), 1.0, material2)
+  world.push Sphere.new(Point3.new(-4,1,0.0), 1.0, material2)
 
   material3 = Metal.new(Color.new(0.7,0.6,0.5), 0.0)
   world.push Sphere.new(Point3.new(4,1,0), 1.0, material3)
@@ -117,25 +120,30 @@ class Renderer
   end
 
   def ppm
-    # Clear before printing status output
-    $stderr.print "\e[2J"
+    pixels = (width * height).times.map do |n|
+      i = n % width
+      j = height - (n / width).ceil - 1
+
+      start_time = Time.now
+      pixel = yield i, j
+
+      $stderr.print progress(i, j, start_time)
+      $stderr.flush
+
+      pixel
+    end
 
     # PPM header
-    $stdout.puts "P3\n#{width} #{height}\n255"
-
-    (height - 1).downto(0) do |j|
-      0.upto(width - 1) do |i|
-        start_time = Time.now
-        $stdout.puts yield i, j
-
-        $stderr.print progress(i, j, start_time)
-        $stderr.flush
-      end
-    end
+    $stdout.puts <<~PPM
+      P3
+      #{width} #{height}
+      255
+      #{pixels.join("\n")}
+    PPM
   end
 
   def progress(i, j, start_time)
-    complete = (height - j) * width + (i + 1)
+    complete = (height - j) * width + i
     percent = (complete.to_f / total.to_f * 100)
     remaining = (Time.now - start_time) * (total - complete)
     "\e[0;0H⏳ Rendering #{"%4.2f" % percent}%, estimated remaining: #{"%3d" % remaining} sec...\e[K"
